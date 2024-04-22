0fb6761e762f7c9eac20e880adb9e6820840344f
tuly15 <momotatuly15@gmail.com>
2024-04-23 03:11:48 +0600

    add redirect file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUoU1Lv5KwlFUjqvXhnofSf2mwBhUgcCI",
-  authDomain: "real-estate-project-ec17b.firebaseapp.com",
-  projectId: "real-estate-project-ec17b",
-  storageBucket: "real-estate-project-ec17b.appspot.com",
-  messagingSenderId: "104471238905",
-  appId: "1:104471238905:web:345fd0c6f3056dbaa1ea28"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID ,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId:import.meta.env. VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
